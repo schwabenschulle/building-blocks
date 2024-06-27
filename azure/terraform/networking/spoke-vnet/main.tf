e36e64241aa8bb00aa2c0d08c4ac9dd84dbcf306
rcf8fe <frank.schulze2@de.bosch.com>
2024-06-27 16:54:31 +0200

    update vnet peering remote gateway

--- azure/terraform/networking/spoke-vnet/main.tf
+++ azure/terraform/networking/spoke-vnet/main.tf
@@ -94,7 +94,7 @@ resource "azurerm_virtual_network_peering" "spoke-hub-peer" {
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
   allow_gateway_transit        = false
-  use_remote_gateways          = true
+  use_remote_gateways          = false
   depends_on                   = [azurerm_virtual_network.spoke-vnet]
 }
 
